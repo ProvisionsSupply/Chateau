ba5668cc95486fe44c65ebaebde32f146a0b71e4
BrandonGul <bsguly@gmail.com>
2024-08-23 17:30:32 -0500

    Added in posthog

---
+++ src/js/posthog.js
@@ -0,0 +1,6 @@
+import posthog from 'posthog-js'
+
+posthog.init('phc_CMS2tAIUUd1IpQZZDRxAivb479JptaGzoNhOK8YtB05',{
+  api_host: 'https://us.i.posthog.com',
+  person_profiles: 'identified_only'
+});
