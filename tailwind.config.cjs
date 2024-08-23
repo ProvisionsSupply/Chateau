3e9f17bbc8cf8ab1b048742fba7e3fa06c205eca
BrandonGul <bsguly@gmail.com>
2024-08-23 18:58:19 -0500

    Added in homepage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,12 @@ module.exports = {
     container: {
       center: true,
       padding: "1rem",
+      screens: {
+        sm: '100%',
+        md: '100%',
+        lg: '100%',
+        xl: '1200px',
+      }
     },
     extend: {
       colors: {
